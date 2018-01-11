4fe7ffd882f594651859c99af994da97d2ca8ddf
dbdennot8 <dennisbmithamo@gmail.com>
2018-01-11 15:15:31 +0300

    Refactor the date format, refine the design

--- assets/js/todolist.js
+++ assets/js/todolist.js
@@ -3,8 +3,8 @@
 function currentTimeDate() {
     var now = moment();
     $("#time-now").text(now.format("HH : mm"));
-    $("#date-today").text(now.format("ddd YYYY-MM-DD"));
-    $("#date-showing").text(now.format("YYYY-MM-DD"));
+    $("#date-today").text(now.format("ddd DD-MM-YYYY"));
+    $("#date-showing").text(now.format("ddd DD-MM-YYYY"));
     checkIfLi();
 }
 // Change time (and incidentally date) every one second
@@ -12,14 +12,14 @@ setInterval(currentTimeDate, 100);
 
 // Add functionality top add-button
 // Display text input field on click
-$("#list-plus").on("click", function(){
+$("#list-plus").on("click", function () {
     $("input").css("display", "block")
 })
 
 // Append footer for empty todo list
-function checkIfLi(){
+function checkIfLi() {
     var presentTodos = $("li").html();
-    if(presentTodos === undefined){
+    if (presentTodos === undefined) {
         $("#list-footer").text("You have no to-dos");
         $("ol").css("padding", "0");
     }
