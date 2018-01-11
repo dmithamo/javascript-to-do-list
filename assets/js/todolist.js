8d99056262fae3cf84c5481437fee1041526a641
dbdennot8 <dennisbmithamo@gmail.com>
2018-01-11 13:58:02 +0300

    Set up footer for empty todo list

--- assets/js/todolist.js
+++ assets/js/todolist.js
@@ -1,12 +1,26 @@
 // Display urrent date and time. Using a library called moment.js
-function currentTimeDate(){
+// Gotta love moment.js :)
+function currentTimeDate() {
     var now = moment();
     $("#time-now").text(now.format("HH : mm"));
     $("#date-today").text(now.format("ddd YYYY-MM-DD"));
     $("#date-showing").text(now.format("YYYY-MM-DD"));
+    checkIfLi();
 }
 // Change time (and incidentally date) every one second
-setInterval(currentTimeDate, 10);
+setInterval(currentTimeDate, 100);
 
-// Gotta love moment.js :)
+// Add functionality top add-button
+// Display text input field on click
 
+// alert($("#list-plus"));
+
+function checkIfLi(){
+    var presentTodos = $("li").html();
+    if(presentTodos === undefined){
+        $("#list-footer").text("You have no to-dos");
+        $("ol").css("padding", "0");
+    }
+}
+
+// "<li><span class='delete'></span>" + Item + "<span class='time'>" + timeDue + "</span></li>"
